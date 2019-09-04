4f40f70fd5391757a0d7bde9e33caab175d70cff
Jack de Grave <jack.degrave@outlook.com>
2019-09-04 15:35:21 +0100

    completed again

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.checked === 'Alphabetically'} onChange={(event) => props.handleSortChange(event)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.checked === 'Price'}  onChange={(event) => props.handleSortChange(event)}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(event) => props.handleDropdownChange(event)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,80 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+
+
+
 class MainContainer extends Component {
+
+  state = {
+    stocks: [],
+    portfolio: [],
+    sortType: null,
+    dropDownType: ""
+  }
+
+  fetchStocks = () => {
+    return fetch('http://localhost:3000/stocks')
+    .then(resp => resp.json())
+  }
+
+  componentDidMount() {
+    this.fetchStocks()
+    .then(stocks => this.setState({ stocks }))
+  }
+
+  buyStock = (stock) => {
+    if (!this.state.portfolio.includes(stock)) {
+      this.setState({portfolio: [...this.state.portfolio, stock]})
+    }
+  }
+
+  sellStock = (s) => {
+   this.setState({portfolio: this.state.portfolio.filter(stock => stock !== s)})
+  }
+
+  sortAndFilterStocks = () => {
+    if (this.state.sortType === 'Alphabetically') {
+      return this.state.stocks.sort((a, b) => (a.ticker).localeCompare(b.ticker))
+    } else if (this.state.sortType === 'Price') {
+      return this.state.stocks.sort((a, b) => a.price - b.price)
+    } else if (this.state.dropDownType === 'Tech') {
+      return this.state.stocks.filter(stock => stock.type === 'Tech')
+    } else if (this.state.dropDownType === 'Finance') {
+      return this.state.stocks.filter(stock => stock.type === 'Finance')
+    } else if (this.state.dropDownType === 'Sportswear') {
+      return this.state.stocks.filter(stock => stock.type === 'Sportswear')
+    } else {
+      return this.state.stocks
+    }
+  }
+
+  handleSortChange = (event) => {
+    this.setState({sortType: event.target.value})
+  }
+
+  handleDropdownChange = (event) => {
+    this.setState({dropDownType: event.target.value})
+  }
+
+  handleClick = () => {
+    this.setState({sortButtonChecked: !this.state.sortButtonChecked})
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar checked={this.state.sortType} handleSortChange={this.handleSortChange} handleDropdownChange={this.handleDropdownChange}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.sortAndFilterStocks()} buyStock={this.buyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} sellStock={this.sellStock}/>
 
             </div>
           </div>
